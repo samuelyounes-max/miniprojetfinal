1af064d9132f37ec64e3bf42a55cc12d84268200
samuelyounes-max <samuel.younes@mmibordeaux.com>
2026-01-16 18:51:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 function go() {
-    let div = document.getElementById('jeu'), msg = document.getElementById('msg');
-    div.innerHTML = msg.innerText = ''; // Reset
-    let tab = Array.from({length:5}, () => Math.random()*100|0), max = Math.max(...tab);
+    let div = document.getElementById('jeu');
+    let msg = document.getElementById('msg');
     
+    // On vide le jeu et le message précédent
+    div.innerHTML = ''; 
+    msg.innerText = ''; 
+
+    // On crée 5 nombres et on trouve le max
+    let tab = Array.from({length:5}, () => Math.floor(Math.random() * 100));
+    let max = Math.max(...tab);
+
     tab.forEach(n => {
         let btn = document.createElement('button');
         btn.innerText = n;
-        btn.onclick = () => msg.inne
+        
+        // Gagné / Perdu
+        btn.onclick = () => {
+            if (n === max) {
+                msg.innerText = "Gagné !";
+                msg.style.color = "green"; //  texte en vert
+            } else {
+                msg.innerText = "Perdu...";
+                msg.style.color = "red";   //  texte en rouge
+            }
+        };
+        
+        div.appendChild(btn);
     });
 }
+
+// Lance le jeu 
 go();
